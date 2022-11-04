71c31ab56abe9ca254076bd6a2576996ebbde14c
djbavibeats <justin@voltcreative.com>
2022-11-04 16:05:46 -0400

    Initial commit

--- src/Experience.js
+++ src/Experience.js
@@ -26,7 +26,7 @@ export default function Experience() {
             enablePan={false}
         />
         <Environment preset="dawn" color="#c90225" background={true} blur={1} />
-        {/* <directionalLight position={ [ 0, 2, 3 ] } intensity={ 0.5 } /> */}
+        <directionalLight position={ [ 0, 2, 3 ] } intensity={ 0.5 } />
         <ambientLight intensity={ 0.5 } color="#c90225" />
         <Sky color="#c90225" distance={10} sunPosition={[0, 4, 0]} inclination={0} azimuth={0.25} />
         <Suspense fallback={null}>
@@ -51,11 +51,11 @@ export default function Experience() {
                 floatFactor={ Math.random() }
             />
         </Suspense>
-        <ContactShadows position={[0, -2, 0]} opacity={0.4} scale={20} blur={1.75} far={4.5} />
+        {/* <ContactShadows position={[0, -2, 0]} opacity={0.4} scale={20} blur={1.75} far={4.5} /> */}
         {/* <mesh rotation-x={ Math.PI * -.5 } position-y={-2} scale={[ 10, 10 ]}>
             <planeGeometry  />
         </mesh> */}
-        {/* <directionalLight useHelper position={[ 0, 5, 0 ]} color="darkblue"  intensity={ 2.5 }/> */}
+        <directionalLight useHelper position={[ 0, 5, 0 ]} color="darkblue"  intensity={ 2.5 }/>
         {/* <Reflector resolution={ 512 } args={[50, 50]} position-z="-2.76" /> */}
     </>)
 }
